a8e32ad7480053f27860984daea39ffa1a0c62d7
emiltriest <emil@triest.de>
2022-03-08 12:44:56 +0100

    adding placebo webapp

--- commands/update.js
+++ commands/update.js
@@ -2,7 +2,7 @@ import {Telegraf} from "telegraf";
 import bot from "../bot.js";
 
 const update = () => {
-    bot.command('update', Telegraf.reply('λ'));
+    bot.command('update', Telegraf.reply("test"));
 }
 
 export default update;
--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import bot from "./bot.js";
 import update from "./commands/update.js";
 
+import http from "http";
+
+// only to fool heroku, making the application seem like a webapp
+const server = http.createServer();
+server.listen(process.env.PORT == null ? 5000 : process.env.PORT);
+
 bot.launch().catch(err => console.log(err));
 
 update();
