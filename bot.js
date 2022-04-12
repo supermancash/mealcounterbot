8ede497ef6e9296c559ff2a76d0a33b818d5b83e
emiltriest <emil@triest.de>
2022-04-12 20:10:56 +0200

    adding bot middleware

--- bot.js
+++ bot.js
@@ -1,41 +1,40 @@
 import UserSchema from "./dao/models/User.js";
 import CounterSchema from "./dao/models/Counter.js";
 
-import {Scenes, Telegraf} from "telegraf";
+import {Scenes, session, Telegraf} from "telegraf";
 
 
 // bot initialisation
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
 bot.start(async (ctx) => {
-        // saving user info to users table
-        const user = new UserSchema(ctx.update.message.from);
-        user.save().catch(err => console.log(err));
+    // saving user info to users table
+    const user = new UserSchema(ctx.update.message.from);
+    user.save().catch(err => console.log(err));
 
-        // saving user info to counters table
-        const counter = new CounterSchema(
-            {
-                id: ctx.update.message.from.id,
-                first_name: ctx.update.message.from.first_name,
-                meals_owed: []
-            }
-        );
-        counter.save().catch(err => console.log(err));
+    // saving user info to counters table
+    const counter = new CounterSchema(
+        {
+            id: ctx.update.message.from.id,
+            first_name: ctx.update.message.from.first_name,
+            meals_owed: []
+        }
+    );
+    counter.save().catch(err => console.log(err));
 
-        // welcome message
-        await ctx.replyWithMarkdown("Hi " + ctx.update.message.from.first_name +
-            "! \n\nWelcome to the `mealcounterbot`, I'll be happy to assist with counting how many meals are owed by " +
-            "colleagues in your team :)" +
-            "\n\n*Use these commands to control me:*" +
-            "\n\n/show - show the current status of owed meals" +
-            "\n/won - record a new bet that was won" +
-            "\n/lost - record a new bet that was lost" +
-            "\n/payup - record a new meal and decrement the list" +
-            "\n/proof - show the list of pictures of past meals" +
-            "\n/help - show the list of commands and their functions (or just type / to see them)"
-        );
-    }
-);
+    // welcome message
+    await ctx.replyWithMarkdown("Hi " + ctx.update.message.from.first_name +
+        "! \n\nWelcome to the `mealcounterbot`, I'll be happy to assist with counting how many meals are owed by " +
+        "colleagues in your team :)" +
+        "\n\n*Use these commands to control me:*" +
+        "\n\n/show - show the current status of owed meals" +
+        "\n/won - record a new bet that was won" +
+        "\n/lost - record a new bet that was lost" +
+        "\n/payup - record a new meal and decrement the list" +
+        "\n/proof - show the list of pictures of past meals" +
+        "\n/help - show the list of commands and their functions (or just type / to see them)"
+    );
+});
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
@@ -45,3 +44,6 @@ export default bot;
 
 // provide for scenes (telegram commands that need a dialogue)
 export const stage = new Scenes.Stage();
+
+bot.use(session());
+bot.use(stage.middleware());
